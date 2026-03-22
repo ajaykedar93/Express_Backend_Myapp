6dd0e3cd156093f1d5008098fb1e085eaee35573
ajaykedar123 <ajaykedar3790@gmail.com>
2026-03-22 14:41:44 +0530

    Add new code react

--- server.js
+++ server.js
@@ -170,7 +170,6 @@ app.use((err, _req, res, _next) => {
 
 /* ---------------- Start Server ---------------- */
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
